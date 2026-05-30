a70ca6344c3d6dde87c0bd6fdb72dabda6a395de
Alrazter <alrazter@gmail.com>
2026-05-30 19:01:55 +0700

    Ganti Judul Web

--- resources/views/user/hero.blade.php
+++ resources/views/user/hero.blade.php
@@ -2,7 +2,7 @@
   <div class="hero-overlay"></div>
   <div class="hero-content">
     <div class="hero-welcome">WELCOME TO SISIRU</div>
-    <h1 class="hero-title">Room Booking System</h1>
+    <h1 class="hero-title">Sistem Reservasi Ruangan</h1>
     <h2 class="hero-subtitle">Laboratorium Terpadu Universitas Tanjungpura</h2>
   </div>
 </div>
